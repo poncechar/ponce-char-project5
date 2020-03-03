820d37690afc0529001889ab5358cf1732bbd7fa
poncechar <poncechar@gmail.com>
2020-03-03 16:52:55 -0500

    removed extra icons and fixed minor details ⚒🌿

--- src/Components/PlantTaskList.js
+++ src/Components/PlantTaskList.js
@@ -18,11 +18,13 @@ const plantTaskList = ({
         <div className='taskList'
           key={userKey}
         >
+          <img className='plantTaskLogo' src={require('../assets/hanging-pot.png')} alt='icon of a watering can' />
+          
           <h2><span className='title'>Your Plant Information</span></h2>
             <li><span className='plantInfo'>Name:</span> {plantName}</li>
             <li><span className='plantInfo'>Location:</span> {plantLocation}</li>
             <li><span className='plantInfo'>Species:</span> {plantSpecies}</li>
-          <h2><span className='title'>Task to do:</span></h2>
+          <h2><span className='title'>Tasks to do:</span></h2>
             <li>{taskOne}</li>
             <li>{taskTwo}</li>
             <li>{taskThree}</li>
