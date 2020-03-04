b779c6c08edb6e25984841469cdbc5f44979ab9f
poncechar <poncechar@gmail.com>
2020-03-04 00:49:32 -0500

    Updates

---
+++ precache-manifest.49bd3dd3a6f18259ef7140c8d29d18ca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24615db080c5c984bcb7b77062c99e87",
+    "url": "/ponce-char-project5/index.html"
+  },
+  {
+    "revision": "3f666bfa6e86a2d2e6e0",
+    "url": "/ponce-char-project5/static/css/main.dbef528f.chunk.css"
+  },
+  {
+    "revision": "7721ba3a18341e36dbcd",
+    "url": "/ponce-char-project5/static/js/2.60a0c4c1.chunk.js"
+  },
+  {
+    "revision": "07a707eba59e3e3ed782f77b31f34780",
+    "url": "/ponce-char-project5/static/js/2.60a0c4c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f666bfa6e86a2d2e6e0",
+    "url": "/ponce-char-project5/static/js/main.155aae4e.chunk.js"
+  },
+  {
+    "revision": "ef70bd3a64e2b7539f39",
+    "url": "/ponce-char-project5/static/js/runtime-main.a0e72b11.js"
+  },
+  {
+    "revision": "05dc1e3bb53e44f2194df5d65e9647c6",
+    "url": "/ponce-char-project5/static/media/chlorophytum.05dc1e3b.png"
+  },
+  {
+    "revision": "9af512feba3e240f6c36a34cad3dd31f",
+    "url": "/ponce-char-project5/static/media/fertilizer.9af512fe.png"
+  },
+  {
+    "revision": "a74fff7570f8daebd74069baea5b0b79",
+    "url": "/ponce-char-project5/static/media/hanging-pot.a74fff75.png"
+  },
+  {
+    "revision": "0e32ec777418d496a5b3c7586f33d3a2",
+    "url": "/ponce-char-project5/static/media/location.0e32ec77.png"
+  },
+  {
+    "revision": "6a7e8e67486973229b64a1a134e5df0d",
+    "url": "/ponce-char-project5/static/media/mist.6a7e8e67.png"
+  },
+  {
+    "revision": "872e4d336cb650b95517dd1899075e9c",
+    "url": "/ponce-char-project5/static/media/monstera.872e4d33.png"
+  },
+  {
+    "revision": "0309857151fe5fffbf66db70af18034b",
+    "url": "/ponce-char-project5/static/media/pots.03098571.png"
+  },
+  {
+    "revision": "46420a8e9ca6283d8ee0a143490cba30",
+    "url": "/ponce-char-project5/static/media/watering-can.46420a8e.png"
+  }
+]);
