d44da6c4fdec26d4f5e0a12dbb8de7ac5abceaaf
poncechar <poncechar@gmail.com>
2020-03-04 00:47:58 -0500

    Updates

---
+++ precache-manifest.6e28a14b435180d2b1309ed57ea27c27.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0293cfc31a48eb68f229af7143618a38",
+    "url": "/plantparenthood/index.html"
+  },
+  {
+    "revision": "892b09cd51348daa2e5f",
+    "url": "/plantparenthood/static/css/main.dbef528f.chunk.css"
+  },
+  {
+    "revision": "024673db6c1ae3894ecb",
+    "url": "/plantparenthood/static/js/2.681b5a8c.chunk.js"
+  },
+  {
+    "revision": "07a707eba59e3e3ed782f77b31f34780",
+    "url": "/plantparenthood/static/js/2.681b5a8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892b09cd51348daa2e5f",
+    "url": "/plantparenthood/static/js/main.16826e60.chunk.js"
+  },
+  {
+    "revision": "1365777440e74cab5561",
+    "url": "/plantparenthood/static/js/runtime-main.0f175475.js"
+  },
+  {
+    "revision": "05dc1e3bb53e44f2194df5d65e9647c6",
+    "url": "/plantparenthood/static/media/chlorophytum.05dc1e3b.png"
+  },
+  {
+    "revision": "9af512feba3e240f6c36a34cad3dd31f",
+    "url": "/plantparenthood/static/media/fertilizer.9af512fe.png"
+  },
+  {
+    "revision": "a74fff7570f8daebd74069baea5b0b79",
+    "url": "/plantparenthood/static/media/hanging-pot.a74fff75.png"
+  },
+  {
+    "revision": "0e32ec777418d496a5b3c7586f33d3a2",
+    "url": "/plantparenthood/static/media/location.0e32ec77.png"
+  },
+  {
+    "revision": "6a7e8e67486973229b64a1a134e5df0d",
+    "url": "/plantparenthood/static/media/mist.6a7e8e67.png"
+  },
+  {
+    "revision": "872e4d336cb650b95517dd1899075e9c",
+    "url": "/plantparenthood/static/media/monstera.872e4d33.png"
+  },
+  {
+    "revision": "0309857151fe5fffbf66db70af18034b",
+    "url": "/plantparenthood/static/media/pots.03098571.png"
+  },
+  {
+    "revision": "46420a8e9ca6283d8ee0a143490cba30",
+    "url": "/plantparenthood/static/media/watering-can.46420a8e.png"
+  }
+]);
